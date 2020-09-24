00d617bf99ea56bb55c138f0c9c475e7f3fc6813
Ryan Zegray <ryan.zegray@ibm.com>
2020-09-24 15:50:59 -0400

    Init project from mp-starter

---
+++ src/main/java/com/example/microprofile/demo/MicroprofiledemoRestApplication.java
@@ -0,0 +1,11 @@
+package com.example.microprofile.demo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MicroprofiledemoRestApplication extends Application {
+}
